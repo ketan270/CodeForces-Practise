050d5c08b3ce563cf895666eaff5c77a6309e0b2
krishna1584 <146920220+ketan270@users.noreply.github.com>
2025-02-22 17:36:22 +0530

    Practise

---
+++ Watermelon.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Watermelon {
+    public static void main(String[] args){
+        Scanner sr = new Scanner(System.in);
+        int n = sr.nextInt();
+        if(n==2){
+            System.out.print("NO");
+
+        }
+        else if(n%2==0){
+            System.out.print("YES");
+        }
+        else{
+            System.out.print("NO");
+        }
+    }
+}
